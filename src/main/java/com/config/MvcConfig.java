4c8854c15c8af7a98de33b2628a2d6dc8c88b863
jerry <leijunjun18@163.com>
2022-02-22 20:06:09 +0800

    登陆相关

---
+++ src/main/java/com/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author zhanbo
+ * @Description TODO
+ * @Date 2021-12-18 11:37
+ * @Version 1.0
+ **/
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
--- src/main/java/com/seckill/controller/LoginController.java
+++ src/main/java/com/seckill/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.seckill.controller;
 
 import com.seckill.dto.LoginDTO;
 import com.seckill.global.Result;
+import com.seckill.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,9 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/login")
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public String toDoLogin() {
         return "login1";
@@ -25,7 +30,7 @@ public class LoginController {
     @PostMapping
     @ResponseBody
     public Result doLogin(HttpServletResponse response, LoginDTO loginDTO) {
-
+        userService.login(response,loginDTO);
         return Result.success(true);
     }
 }
